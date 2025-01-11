2731b9164577335819e31a246c325a40f4727bb9
tiran <tiran@intrigsoft.com>
2025-01-11 20:48:03 +0530

    feat(insight): Add dynamic schema documentation generation
    Introduce functionality to dynamically generate and render schema
    documentation for types. This includes parsing a schema file, extracting
    metadata, and rendering it as markdown with multiple views (Type, JSON
    Schema, Zod Schema). This improvement supports better documentation and
    enhances development efficiency.

---
+++ apps/web/src/app/sources/[sourceId]/schema/[type]/doc.md
@@ -0,0 +1,20 @@
+{% tabs %}
+{% tab title="Type" %}
+
+```typescript
+{% $typescriptType %}
+```
+{% /tab %}
+{% tab title="JSON Schema" %}
+
+```typescript
+{% $jsonSchema %}
+```
+{% /tab %}
+{% tab title="Zod Schema" %}
+
+```typescript
+{% $zodSchemas %}
+```
+{% /tab %}
+{% /tabs %}
---
+++ apps/web/src/app/sources/[sourceId]/schema/[type]/page.tsx
@@ -0,0 +1,172 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import { Documentation } from '@/components/Documentation';
+import { NextRequest } from 'next/server';
+import { INTRIG_LOCATION } from '@/const/locations';
+import docContent from './doc.md'
+import { camelCase } from '@/lib/change-case';
+
+type ExtractedSections = Record<string, string>;
+
+export const dynamic = 'force-dynamic';
+
+export default async function Index({ params: {sourceId, type}}: {params: {sourceId: string, type: string}}) {
+
+  let typeName = decodeURIComponent(type);
+
+  let filePath = path.resolve(INTRIG_LOCATION, 'generated', 'src', sourceId, 'components', 'schemas', `${typeName}.ts`);
+
+  let content = fs.readFileSync(filePath, 'utf8');
+
+  const regex = /\/\/--- (.+?) ---\/\/\n([\s\S]*?)(?=\/\/---|$)/g;
+
+  const sections: ExtractedSections = {};
+
+  let match;
+  while ((match = regex.exec(content)) !== null) {
+    const [, name, sectionContent] = match;
+    sections[camelCase(name.trim())] = sectionContent.trim();
+  }
+
+  let jsonSchema = sections?.simpleType?.replace("/*[", "").replace("]*/", "").trim();
+
+  let schemaFields = parseSchema(jsonSchema ? JSON.parse(jsonSchema) : {});
+
+  let _content = `---
+title: ${typeName}
+---
+
+## Schema Definition
+
+{% table %}
+---
+* Field
+* Type
+* Description
+* Validations
+* Example
+---
+${schemaFields.map(field => `
+* ${field.field ?? ''}
+* ${field.type ? (field.type.startsWith('#') ? `[${field.type.split('/').pop()}](/sources/${sourceId}/schema/${field.type.split('/').pop()}) {% colspan=4 %}` : field.type) : ''}
+* ${field.description ?? ''}
+* ${field.validations?.length ? `
+  {% table %}
+  ---
+  * property
+  * value
+  ---
+  ${field.validations.map(validation => `
+  * ${validation.key}
+  * ${validation.value}
+  ---
+`)}
+  {% /table %}
+` : ''}
+* ${field.example ?? ''}
+---
+`)}
+{% /table %}
+
+## Schema Representation
+
+{% tabs %}
+{% tab title="Type" %}
+
+\`\`\`typescript
+{% $typescriptType %}
+\`\`\`
+{% /tab %}
+{% tab title="JSON Schema" %}
+
+\`\`\`typescript
+{% $jsonSchema %}
+\`\`\`
+{% /tab %}
+{% tab title="Zod Schema" %}
+
+\`\`\`typescript
+{% $zodSchemas %}
+\`\`\`
+{% /tab %}
+{% /tabs %}
+`
+
+  return (
+    <>
+      <Documentation content={_content} variables={{
+        name: type,
+        ...sections,
+      }}/>
+    </>
+  );
+}
+
+interface Validation {
+  key: string;
+  value: string;
+}
+
+interface SchemaField {
+  field: string;
+  depth: number;
+  jsonPath: string;
+  type: string;
+  validations: Validation[];
+  description?: string;
+  example?: string;
+}
+
+let potentialValidations = [
+  'minLength',
+  'maxLength',
+  'pattern',
+  'enum',
+  'format',
+  'minimum',
+  'maximum',
+  'exclusiveMinimum',
+  'exclusiveMaximum',
+  'minItems',
+  'maxItems',
+  'uniqueItems',
+  'minProperties',
+  'maxProperties',
+  'required',
+  'additionalProperties'
+]
+
+function parseSchema(schema: any, prefix = "", depth = 0, jsonPath = "$"): SchemaField[] {
+  let result: SchemaField[] = [];
+
+  if (schema.type === "object" && schema.properties) {
+    for (const key in schema.properties) {
+      const value = schema.properties[key];
+      const fieldName = `${' '.repeat(depth * 2)}${key}`; // Indentation
+      const fieldJsonPath = `${jsonPath}.${key}`;
+      const fieldType = value.type || value["$ref"] || "unknown";
+
+      const validations = Object.entries(value)
+        .filter(([key]) => potentialValidations.includes(key))
+        .map(([key, value]) => ({
+          key,
+          value: Array.isArray(value) ? value.join(', ') : value + '',
+        }))
+
+      result.push({
+        field: fieldName,
+        depth,
+        jsonPath: fieldJsonPath,
+        type: fieldType,
+        validations,
+        description: value.description,
+        example: value.example,
+      });
+
+      if (value.type === "object") {
+        result = result.concat(parseSchema(value, `${fieldName}.`, depth + 1, fieldJsonPath));
+      }
+    }
+  }
+  return result;
+}
